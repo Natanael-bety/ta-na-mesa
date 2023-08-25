2727da63ab9a4506207c938ccc56a3303644b2df
Natanael Ferreira <ferreiranatanael648@gmail.com>
2023-08-25 18:04:03 -0300

    update chamada

--- src/modules/chamada/chamada.controller.ts
+++ src/modules/chamada/chamada.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { ChamadaService } from './chamada.service';
 import { CreateChamadaDto } from './dto/create-chamada.dto';
+import { UpdateChamadaDto } from './dto/update-chamada.dto';
 
 @Controller('chamadas')
 export class ChamadaController {
@@ -12,5 +13,13 @@ export class ChamadaController {
     @Body() createChamadaDto: CreateChamadaDto,
   ) {
     return this.chamadaService.create(createChamadaDto, mesaId);
+  }
+
+  @Put(':chamadaId')
+  update(
+    @Param('chamadaId') chamadaId: string,
+    @Body() updateChamadaDto: UpdateChamadaDto,
+  ) {
+    return this.chamadaService.update(chamadaId, updateChamadaDto);
   }
 }
--- src/modules/chamada/chamada.service.ts
+++ src/modules/chamada/chamada.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { Chamada } from 'src/models/chamada.model';
 import { CreateChamadaDto } from './dto/create-chamada.dto';
 import { MesaService } from '../mesa/mesa.service';
+import { UpdateChamadaDto } from './dto/update-chamada.dto';
 
 @Injectable()
 export class ChamadaService {
@@ -24,6 +25,29 @@ export class ChamadaService {
       });
 
       return chamadaCreate;
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  async findById(chamadaId: string): Promise<Chamada> {
+    const chamada: Chamada = await this.ChamadaModel.findOne({
+      where: { id: chamadaId },
+    });
+    if (!chamada) {
+      throw new Error('Chamada não encontrada');
+    }
+
+    return chamada;
+  }
+
+  async update(chamadaId: string, { chamadaResolvida }: UpdateChamadaDto) {
+    try {
+      const chamada = await this.findById(chamadaId);
+
+      const updateChamada = chamada.update({ chamadaResolvida });
+
+      return updateChamada;
     } catch (e) {
       throw new BadRequestException(e.message);
     }
