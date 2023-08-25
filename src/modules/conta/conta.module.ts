c2568dccce32b9f29fa0762366d1febbcda180ed
Natanael Ferreira <ferreiranatanael648@gmail.com>
2023-08-25 19:19:44 -0300

    feat: ✨to add mesaModel in contaModule

--- src/modules/conta/conta.module.ts
+++ src/modules/conta/conta.module.ts
@@ -3,10 +3,11 @@ import { ContaService } from './conta.service';
 import { ContaController } from './conta.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Conta } from 'src/models/conta.model';
+import { MesaModule } from '../mesa/mesa.module';
 
 @Module({
   controllers: [ContaController],
   providers: [ContaService],
-  imports: [SequelizeModule.forFeature([Conta])],
+  imports: [SequelizeModule.forFeature([Conta]), MesaModule],
 })
 export class ContaModule {}
